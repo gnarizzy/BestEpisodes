a149facc3583049445f9bc0d4c13199b5df00861
Gautam <gautam@uga.edu>
2016-08-30 22:29:27 -0400

    Home page renders template and passes associated tests

--- BestEpisodes/core/tests.py
+++ BestEpisodes/core/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from core.views import home
+from django.http import HttpRequest
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+
+#Test that episodes are loaded into database correctly
+
+#Test that episodes are selected randomly
+
+#Test that ratings are updated correctly in the case of a win
+
+#Test that ratings are updated correctly in the case of a draw
+
+#Test that ratings aren't updated if user selects pass
+
+
+
+
+
+
+
+
--- BestEpisodes/core/views.py
+++ BestEpisodes/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# Shows two unique, randomly selected episodes with screenshot, title, and description
+def home(request):
+    return render(request, 'home.html')
