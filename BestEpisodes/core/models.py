9ec3020f2d710c86f5c7d2c05ff72a133f4fb767
Gautam <gautam@uga.edu>
2016-08-31 20:45:02 -0400

    Fixed string representation

--- BestEpisodes/core/models.py
+++ BestEpisodes/core/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 class Episode(models.Model):
-    IMDB_ID = models.TextField(default="")
-    series = models.TextField(default="")
+    imdb_id = models.TextField()
+    series = models.TextField()
     season = models.IntegerField(default=0)
-    title = models.TextField(default="")
-    plot = models.TextField(default="")
-    image = models.URLField(default="")
+    episode = models.IntegerField(default=0)
+    title = models.TextField()
+    plot = models.TextField()
+    image = models.URLField()
     rating = models.DecimalField(decimal_places=1, max_digits=5, default=1000)
+
+    def __str__(self):
+        return "Season "+ str(self.season) + " episode " + str(self.episode) + ": " + self.title
 
