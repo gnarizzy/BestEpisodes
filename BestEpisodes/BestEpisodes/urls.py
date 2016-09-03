45be600bbe4c5034a657220ffc1ec40d3ae5b615
Gautam <gautam@uga.edu>
2016-09-03 09:54:38 -0400

    Added about page using generic view

--- BestEpisodes/BestEpisodes/urls.py
+++ BestEpisodes/BestEpisodes/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from core import views as core_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', core_views.home, name='home'),
-    url(r'^rankings/', core_views.rankings, name='rankings'),
+    url(r'^rankings/$', core_views.rankings, name='rankings'),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html"))
 ]
