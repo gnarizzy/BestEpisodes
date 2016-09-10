3b34692fde7623bc14e1487ef32a481f8253938a
Gautam <gautam@uga.edu>
2016-09-10 14:05:23 -0400

    add slugs

--- BestEpisodes/core/models.py
+++ BestEpisodes/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Episode(models.Model):
@@ -11,9 +12,14 @@ class Episode(models.Model):
     image_src = models.URLField()
     image = models.ImageField()
     rating = models.DecimalField(decimal_places=1, max_digits=5, default=1000)
+    slug = models.SlugField()
 
     def __str__(self):
         return "Season "+ str(self.season) + " episode " + str(self.episode) + ": " + self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Episode, self).save(*args, **kwargs)
 
 class Game(models.Model):
     player1 = models.ForeignKey(Episode, related_name='Games_Player1')
