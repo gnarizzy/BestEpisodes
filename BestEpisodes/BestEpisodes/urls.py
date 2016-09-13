f893669909dae73d376cc1bd4cff7860db7edc5c
Gautam <gautam@uga.edu>
2016-09-13 14:36:42 -0400

    remove season rankings url

--- BestEpisodes/BestEpisodes/urls.py
+++ BestEpisodes/BestEpisodes/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     url(r'^rankings/$', core_views.rankings, name='rankings'),
     url(r'^episode/(?P<episode_id>\d+)/$', core_views.episode_detail_no_slug, name='episode_detail_no_slug'),
     url(r'^episode/(?P<episode_id>\d+)/(?P<episode_slug>[\w\-]+)/$', core_views.episode_detail, name='episode_detail'),
-    url(r'^rankings/season/$', core_views.season_rankings, name='season_rankings'),
     url(r'^about/$', TemplateView.as_view(template_name="about.html")),
     url(r'^season/(?P<season_id>\d+)/$', core_views.season_detail, name='season_detail'),
 
