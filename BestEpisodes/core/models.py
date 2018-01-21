e5a6a7a4f63c15352055f0ce6138a3310f760170
Gautam Narula <gautam@gautamnarula.com>
2018-01-20 20:34:25 -0500

    Update models to reflect songs and albums

--- BestEpisodes/core/models.py
+++ BestEpisodes/core/models.py
@@ -2,29 +2,41 @@ from django.db import models
 from django.utils.text import slugify
 
 
-class Episode(models.Model):
-    imdb_id = models.TextField()
-    series = models.TextField()
-    season = models.IntegerField(default=0)
-    episode = models.IntegerField(default=0)
-    title = models.TextField()
-    plot = models.TextField()
+class Album(models.Model):
+    title = models.CharField(max_length=160)
+    artist = models.CharField(max_length=160)
     image_src = models.URLField()
-    image = models.ImageField(upload_to="/images", blank=True, null=True)
+    image = models.ImageField(upload_to="images", blank=True, null=True)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Album, self).save(*args, **kwargs)
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    title = models.CharField(max_length=160)
+    artist = models.CharField(max_length=160)
+    image_src = models.URLField()
+    image = models.ImageField(upload_to="images", blank=True, null=True)
     rating = models.DecimalField(decimal_places=1, max_digits=5, default=1000)
+    year = models.IntegerField()
     slug = models.SlugField()
     stream_url = models.URLField(null=True, blank=True)
 
     def __str__(self):
-        return "Season "+ str(self.season) + " episode " + str(self.episode) + ": " + self.title
+        return self.title
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
-        super(Episode, self).save(*args, **kwargs)
+        super(Song, self).save(*args, **kwargs)
 
 class Game(models.Model):
-    player1 = models.ForeignKey(Episode, related_name='Games_Player1')
-    player2 = models.ForeignKey(Episode, related_name='Games_Player2')
+    player1 = models.ForeignKey(Song, related_name='Games_Player1', on_delete=models.CASCADE)
+    player2 = models.ForeignKey(Song, related_name='Games_Player2', on_delete=models.CASCADE)
     result = models.DecimalField(decimal_places=1, max_digits=2)
     player1_pre = models.DecimalField(decimal_places=1, max_digits=5)
     player1_post = models.DecimalField(decimal_places=1, max_digits=5)
@@ -34,8 +46,6 @@ class Game(models.Model):
     player2_delta = models.DecimalField(decimal_places=1, max_digits=5)
 
     def __str__(self):
-        return "Game " + str(self.id) + " "+ self.player1.title + " vs " + self.player2.title
+        return "Game {0}: {1} vs {2}".format(str(self.id), self.player1.title, self.player2.title)
 
 
-class Season(models.model):
-    stream_url = models.URLField(null=True, blank=True)
