59be0faae434337829997e83e1a256ca0ee96eae
Gautam Narula <gautam@gautamnarula.com>
2018-01-20 20:35:19 -0500

    register newly named models with admin

--- BestEpisodes/core/admin.py
+++ BestEpisodes/core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from core.models import Episode, Game
+from core.models import Song, Game, Album
 
-admin.site.register(Episode)
+admin.site.register(Album)
+admin.site.register(Song)
 admin.site.register(Game)
+
